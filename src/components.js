fdca38352c8ce1a96d444127319c56645d896fa1
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-06-04 02:37:04 +0700

    improve message

--- src/components.js
+++ src/components.js
@@ -1,14 +1,19 @@
-function showMessage($message, $type = "error", $duration = 2000) { //message component
-    if ($type === "success") {
-      document.getElementById("message-container").style.backgroundColor =
-        "#4BB543";
-    } else {
-      document.getElementById("message-container").style.backgroundColor =
-        "#FF0000";
-    }
-    document.getElementById("message-container").style.display = "flex";
-    document.getElementById("message").innerText = $message;
-    setTimeout(() => {
-      document.getElementById("message-container").style.display = "none";
-    }, $duration);
+function showMessage($message, $type = "error", $duration = 2000) {
+  //message component
+  if ($type === "success") {
+    document.getElementById("message-container").style.backgroundColor =
+      "#4BB543";
+  } else {
+    document.getElementById("message-container").style.backgroundColor =
+      "#FF0000";
   }
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  document.getElementById("message-container").style.display = "flex";
+  document.getElementById("message").innerText = $message;
+  setTimeout(() => {
+    document.getElementById("message-container").style.display = "none";
+  }, $duration);
+}
